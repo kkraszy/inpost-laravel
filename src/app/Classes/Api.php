02504b07837bfdcc9319d1cb3e853f0ceb3538e1
Grzegorz Adamczyk <naturalgroove@gmail.com>
2024-08-03 17:32:57 +0200

    Added checking for the api key in the Api class

--- src/app/Classes/Api.php
+++ src/app/Classes/Api.php
@@ -10,15 +10,23 @@ class Api
 
     protected static ?string $dynamicApiKey = null;
 
-    protected string $apiKey, $url;
+    protected ?string $apiKey;
+    protected string $url;
 
-    public static function setApiKey(string $apiKey): void {
+    public static function setApiKey(string $apiKey): void
+    {
         self::$dynamicApiKey = $apiKey;
     }
 
-    public function __construct() {
+    public function __construct()
+    {
         // set api key from config or dynamic
         $this->apiKey = self::$dynamicApiKey ?? config('inPost.api_key');
         $this->url = config('inPost.sandbox') ? config('inPost.sandbox_url') : config('inPost.api_url');
+
+        // check if api key is set
+        if (is_null($this->apiKey)) {
+            throw new \Exception('InPost API key is not set.');
+        }
     }
 }
