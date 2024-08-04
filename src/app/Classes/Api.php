c1e1a30269821b454879df913afaf35c09604510
Grzegorz Adamczyk <naturalgroove@gmail.com>
2024-08-04 18:28:10 +0200

    Added funtionality to set sandbox mode in the API class

--- src/app/Classes/Api.php
+++ src/app/Classes/Api.php
@@ -9,9 +9,15 @@ class Api
     use functions;
 
     protected static ?string $dynamicApiKey = null;
+    protected static ?bool $sandbox = null;
 
     protected ?string $apiKey;
     protected string $url;
+
+    public static function setSandbox(bool $sandbox = true): void
+    {
+        self::$sandbox = $sandbox;
+    }
 
     public static function setApiKey(string $apiKey): void
     {
@@ -20,9 +26,11 @@ class Api
 
     public function __construct()
     {
+        $this->sandbox = (self::$sandbox === null) ? config('inPost.sandbox') : self::$sandbox;
+
         // set api key from config or dynamic
         $this->apiKey = self::$dynamicApiKey ?? config('inPost.api_key');
-        $this->url = config('inPost.sandbox') ? config('inPost.sandbox_url') : config('inPost.api_url');
+        $this->url =  $this->sandbox ? config('inPost.sandbox_url') : config('inPost.api_url');
 
         // check if api key is set
         if (is_null($this->apiKey)) {
